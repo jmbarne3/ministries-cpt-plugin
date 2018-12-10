ebbb63044d69dba2423c2ab084383a90b039aeec
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-12-10 13:51:55 -0500

    Initial commit

---
+++ ministry-cpt.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Ministry Custom Post Type
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/jmbarne3/ministry-cpt-plugin/
+ * Author: Jim Barnes
+ * Author URI: https://github.com/jmbarne3/
+ * Description: Provides a custom post type and shortcodes for ministries.
+ * Github Plugin URI: jmbarne3/ministry-cpt-plugin
+ */
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
